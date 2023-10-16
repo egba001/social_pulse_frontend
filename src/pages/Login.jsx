a5f2285a09dca569c1c38fa0fd03d5c8c6ea2877
William <anazawilliam1@gmail.com>
2023-10-16 12:15:43 +0100

    register overflow

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,8 @@ const Login = () => {
   const [checked, setChecked] = useState(false);
   const [viewPassword, setViewPassword] = useState(false);
   return (
-    <section className="login-container grid grid-col-1 md:grid-cols-4 gap-10 md:gap-0 min-h-screen">
-      <aside className="col-span-4 md:col-span-1 flex items-center justify-center bg-green  h-80 md:h-full">
+    <section className="login-container grid grid-col-1 md:grid-cols-4 gap-10 md:gap-0 min-h-screen overflow-y-scroll">
+      <aside className="hidden md:col-span-1 md:flex items-center justify-center bg-green  h-80 md:h-full">
         <Link to="/" className="logo">
           <img
             src={socialPulse}
