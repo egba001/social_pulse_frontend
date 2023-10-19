65d7ada36eedee6752f9207e00330102cc4354e0
William <anazawilliam1@gmail.com>
2023-10-19 21:49:46 +0100

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import LandingPage from "./pages/LangingPage";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const currentUser = useContext(AuthContext);
+  // console.log(currentUser);
+
+  // protected route to check for user
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route
+          index
+          element={<ProtectedRoute></ProtectedRoute>}
+        /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,77 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import socialPulse from "../assets/Logo.png";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// firebase
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref } from "firebase/storage";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
 const Register = () => {
   const [viewPassword, setViewPassword] = useState(false);
+
+  const [fullname, setFullname] = useState("");
+  const [businessName, setBusinessName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [disable, setDisable] = useState(true);
+
+  // route
+  const navigate = useNavigate();
+
+  // function to reset all the input fields
+  const resetFields = () => {
+    setFullname("");
+    setBusinessName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  // change the disable state of the button
+  useEffect(() => {
+    setDisable(
+      password !== confirmPassword || password === "" || confirmPassword === ""
+    );
+  }, [password, confirmPassword]);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const displayName = businessName;
+    console.log(displayName);
+    //
+    const storageRef = ref(storage, displayName);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      console.log(res.user);
+
+      // Add a new document in collection "user" = Firebase/Firestore
+      // the password is not saved in the db because the user collection will be used to see other users
+      await setDoc(doc(db, "users", res.user.uid), {
+        uid: res.user.uid,
+        businessName,
+        fullname,
+        email,
+        createdAt: serverTimestamp(),
+      });
+
+      // reset all the input fields
+      resetFields();
+
+      // navigate to dashboard
+      navigate("/");
+
+      console.log("user created!");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <section className="login-container grid grid-col-1 md:grid-cols-4 gap-10 md:gap-0 min-h-screen overflow-y-scroll">
@@ -22,19 +89,39 @@ const Register = () => {
           <h2 className="text-center text-dark text-3xl md:text-4xl font-semibold mb-5">
             Create an Account
           </h2>
-          <form className="form m-5">
+          <form className="form m-5" onSubmit={handleRegister}>
             <label className="label">FullName</label>
-            <input type="text" className="input-field" placeholder="John Doe" />
+            <input
+              type="text"
+              className="input-field"
+              placeholder="John Doe"
+              onChange={(e) => setFullname(e.target.value)}
+              value={fullname}
+            />
             <label className="label">Business Name</label>
-            <input type="text" className="input-field" placeholder="" />
+            <input
+              type="text"
+              className="input-field"
+              placeholder=""
+              onChange={(e) => setBusinessName(e.target.value)}
+              value={businessName}
+            />
             <label className="label">Business Email Address</label>
-            <input type="email" className="input-field" placeholder="" />
+            <input
+              type="email"
+              className="input-field"
+              placeholder=""
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
             <label className="label">Password</label>
             <div className="password relative h-[40px] rounded">
               <input
                 type={viewPassword ? "text" : "password"}
                 className="input-field h-full"
                 placeholder=""
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
               />
               {viewPassword ? (
                 <AiFillEyeInvisible
@@ -54,6 +141,8 @@ const Register = () => {
                 type={viewPassword ? "text" : "password"}
                 className="input-field h-full"
                 placeholder=""
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
               />
               {viewPassword ? (
                 <AiFillEyeInvisible
@@ -73,7 +162,14 @@ const Register = () => {
               <span className="text-green">Terms of Service</span> and{" "}
               <span className="text-green">Privacy Policy</span>
             </p>
-            <button className="block w-full bg-green text-white text-base font-medium outline-none border-none py-2 my-6 rounded hover:bg-green">
+            <button
+              className={
+                disable
+                  ? "block w-full bg-gray-400 text-white text-base font-medium outline-none border-none py-2 my-6 rounded cursor-not-allowed"
+                  : "block w-full bg-green text-white text-base font-medium outline-none border-none py-2 my-6 rounded hover:bg-green cursor-pointer"
+              }
+              disabled={disable}
+            >
               Sign Up
             </button>
             <p className="block text-center bg-[#E0F4DF] font-red font-bold text-sm py-2">
